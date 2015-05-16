9efdabfddefc17edccc68abbd14dbdd57e0310ff
mudroljub <mudroljub@gmail.com>
2015-05-16 23:12:15 +0200

    razdvojeno klasa Karakter

--- js/igrica.js
+++ js/igrica.js
@@ -48,9 +48,6 @@ var karakteri = [];
 
 var nivo1 = new Scena('platno');
 
-
-
-
 var likovi = {                           // nazivi su bitni, od njih pravi objekte
     vulin: 'slike/vulin.png',
     toma: 'slike/toma.png',
@@ -174,93 +171,4 @@ function brisiPoruke(){
 
 
 /*************** KLASE ***************/
-
-/* prima sliku i scenu, pravi novog lika */
-function Karakter(slika_src, scena){
-    this.uveden_u_igru = false;
-    this.ostavlja_poruku = false;
-    this.spust = 0;
-    this.spustanje = false;
-
-    /*
-    // prepisano iz simple game, upotrebiti!
-    this.scena = scena; // scenu proslediti kao argument
-     this.visinaSveta = parseInt(this.platno.height);
-     this.sirinaSveta = parseInt(this.platno.width);
-*/
-
-    this.platno = scena.platno;
-    this.sadrzaj = scena.sadrzaj;
-    // prepraviti da ide preko scene
-    //this.platno = scena.platno;
-    //this.sadrzaj = this.platno.getContext("2d");
-
-    this.slika = new Image();
-    this.slika.src = slika_src;
-    // prilagodjava sliku bazicnoj velicini slike
-    this.slika.width = this.slika.width / (this.slika.height / BAZICNA_VISINA_SLIKE);
-    this.slika.height = BAZICNA_VISINA_SLIKE;
-    // prilagodjava sliku trenutnom ekranu
-    this.slika.width = this.slika.width * (window.innerWidth/BAZICNA_SIRINA_EKRANA);
-    this.slika.height = this.slika.height * (window.innerWidth/BAZICNA_SIRINA_EKRANA);
-
-    // prima visinu i sirinu od svoje slike
-    this.sirina = this.slika.width;
-    this.visina = this.slika.height;
-
-    /* uzima slucajne koordinate i pripisuje sebi */
-    this.slucajnaPozicija = function() {
-        this.x = slucajniProzor()[0];
-        this.y = slucajniProzor()[1];
-    }   // slucajnaPozicija
-
-
-    this.goreDole = function(){
-        // lagano se spusta i dize
-        if(this.spust >= 100) {
-            this.spustanje = true;
-        } else if (this.spust <= 0) {
-            this.spustanje = false;
-        }
-        this.spustanje ? this.spust-- : this.spust++;
-    }
-
-    /* crta sebe na trenutnim ili zadatim koordinatama */
-    this.crtaj = function() {
-        this.sadrzaj.drawImage(this.slika, this.x, this.y, this.sirina, this.visina);
-    }   // kraj crtaj
-
-
-    /* proverava jel drugi karakter zauzeo njegovu poziciju */
-    this.jelDrugiZauzeo = function(drugi_karakter){
-        if(this.x == drugi_karakter.x && this.y == drugi_karakter.y){
-            return true;
-        } else return false;
-    }   // kraj jelDrugiZauzeo
-
-
-    /* prima niz likova, proverava dal su zauzeli njegovu poziciju, vraca jel slobodna */
-    this.jelOvoSlobodno = function (ostali) {
-        var index = ostali.indexOf(this);
-        ostali.splice(index, 1);    // izbacuje sebe
-
-        var slobodno = true;
-        // ide redom kroz ostale i proverava sudar
-        for (var i = 0; i < ostali.length; i++) {
-            slobodno = slobodno & !this.jelDrugiZauzeo(ostali[i]);
-        }
-        ostali.push(this);          // vraca sebe
-        return slobodno;
-    }   // kraj jelOvoSlobodno
-
-
-    /* uzima slucajnu poziciju, ako je zauzeta, uporno trazi slobodnu */
-    this.nadjiSlobodnuPoziciju = function (ostali) {
-        this.slucajnaPozicija();
-        while ( !this.jelOvoSlobodno(ostali) ) {
-            this.slucajnaPozicija();
-        }
-    }   // kraj naSlobodnomCrtaj
-
-}   // kraj klase Karakter
 
