6c8d7edbff608de32fb4c9a15ccec8d42f52a6d1
mudroljub <mudroljub@gmail.com>
2015-05-20 18:08:10 +0200

    alatke

--- js/alatke.js
+++ js/alatke.js
@@ -3,7 +3,16 @@
 window.$ = function(selector) {
     return document.querySelector(selector);
 };
+
 window.$$ = function(selector) {
     return document.querySelectorAll(selector);
 };
+
+function uzmi(element){
+    return document.getElementById(element);
+}
+
+function kazi(bilo_sta){
+    console.log(bilo_sta);
+}
 
