3323ccc4cf67cc154f8c1dfd3a3ee3c483d91564
mudroljub <mudroljub@gmail.com>
2015-05-26 20:54:48 +0200

    Ucitavac radi!

--- js/klasaUcitavac.js
+++ js/klasaUcitavac.js
@@ -5,33 +5,35 @@ function Ucitavac() {
     var sve_slike = 0;
     var ucitane_slike = 0;
 
-    this.ucitajSlike = function(slike, povratnaRadnja){     // nakon učitavanja obično pušta uvod
-        var glavni_kljucevi = Object.keys(slike)
-        var ukupno_kljuceva = Object.keys(slike).length
 
-        for(var i=0; i < ukupno_kljuceva; i++) {
-            var grupe_slika = glavni_kljucevi[i];
-            var ova_grupa = slike[grupe_slika];
-            var slika_u_grupi = Object.keys(ova_grupa).length;
-            sve_slike += slika_u_grupi
+    this.ucitajSlike = function(slike, povratnaRadnja){     // nakon učitavanja obično pušta uvod
+        var nazivi_grupa = Object.keys(slike)
+        var ukupno_grupa = Object.keys(slike).length
+
+        for(var i=0; i < ukupno_grupa; i++) {
+            var naziv_grupe = nazivi_grupa[i];
+            var ova_grupa = slike[naziv_grupe];
+            var broj_slika_u_grupi = Object.keys(ova_grupa).length;
+            sve_slike += broj_slika_u_grupi
 
             for(var ime_slike in ova_grupa){
                 var ova_slika = new Image()
                 ova_slika.onload = function kadUcitaSliku (){
                     ovaj_ucitavac.proveriUcitano(povratnaRadnja);
-                } // kraj kadUcitaSliku
+                } 
                 ova_slika.src = ova_grupa[ime_slike]
             } // kraj for in
         }	// kraj for
     }	// kraj ucitajSlike
 
+
     this.proveriUcitano = function(povratnaRadnja){
         ucitane_slike++;
-        kazi("ucitane_slike: " + ucitane_slike)
         if(ucitane_slike >= sve_slike) {
             povratnaRadnja();
         }
     }	// proveriUcitano
+
 
     this.praviKaraktere = function (likovi, scena, vreme){
         for (var lik in likovi){
