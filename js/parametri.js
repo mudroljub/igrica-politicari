d5569825b8ff464716a0b70c896a5033ab1d9f95
mudroljub <mudroljub@gmail.com>
2015-06-24 23:45:48 +0200

    3d verzija radna stabilna

--- js/parametri.js
+++ js/parametri.js
@@ -1,9 +1,9 @@
 var parametri_prozora = {
-    gornji_red: 3.76,
-    donji_red: 1.9,
+    gornji_red: 3.45,
+    donji_red: 1.82,
     
     prvi_niz: 3.4,
     drugi_niz: 2.45,
-    treci_niz: 1.98,
+    treci_niz: 1.95,
     cetvrti_niz: 1.6
 }
