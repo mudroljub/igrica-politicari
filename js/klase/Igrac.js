9090f61285cd2159d578717783ebd733c4cfe079
mudroljub <mudroljub@gmail.com>
2015-07-07 20:51:41 +0200

    random masa

--- js/klase/Igrac.js
+++ js/klase/Igrac.js
@@ -4,17 +4,30 @@
 function Igrac(scena) {
 	this.energija = 10;
 	this.sadrzaj = scena.sadrzaj;
-}
+	this.masa = this.praviMasu();
+	this.VISINA_MASE = 113;	
+	this.sirinaPojedinca = window.innerWidth/this.energija;
+	this.dnoEkrana = window.innerHeight - this.VISINA_MASE;
+}	// Igrac
 
 
-Igrac.prototype.crtaEnergiju = function() {
-	var pojedinac = new Image();
-	pojedinac.src = "slike/politicar.png";
-	var sirinaPodeoka = window.innerWidth/this.energija;
-	var dnoStrane = window.innerHeight-pojedinac.height;
-		
-	for(var i = 0; i < this.energija; i++){
-		this.sadrzaj.drawImage(pojedinac, i * sirinaPodeoka, dnoStrane);		
+Igrac.prototype.crtaEnergiju = function() {	
+	for(var i = 0; i < this.masa.length; i++){
+
+		this.sadrzaj.drawImage(this.masa[i].slika, i * this.sirinaPojedinca + this.masa[i].odstupanje, this.dnoEkrana);
 	}
+}	// crtaEnergiju()
+
+
+Igrac.prototype.praviMasu = function() {
+	var masa = [];
+	for(var i = 0; i < this.energija; i++){
+		var pojedinac = new Image();
+		pojedinac.src = "slike/politicar.png";
+		// prilagodi sliku
+		var odstupanje = Math.random() * 60 - 30;
+		masa.push({slika: pojedinac, odstupanje: odstupanje})
+	}
+	return masa;
 }	// crtaEnergiju()
 
