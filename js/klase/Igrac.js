6a8d58a3be506f92bb0199aa65503272ba44b8f7
mudroljub <mudroljub@gmail.com>
2015-07-10 00:54:51 +0200

    sve radi, ne mogu da prilagodim visinu mase

--- js/klase/Igrac.js
+++ js/klase/Igrac.js
@@ -1,14 +1,30 @@
 // energiju skida kada politicari bacaju parole
 // 100 energije je 50 ljudi
-
 function Igrac(scena) {
 	this.energija = 10;
+	this.standard = 50;	
 	this.sadrzaj = scena.sadrzaj;
 	this.ljudi = this.praviMasu();
 	this.VISINA_MASE = 113;	
-	this.sirinaPojedinca = window.innerWidth/this.energija;
-	this.dnoEkrana = window.innerHeight - this.VISINA_MASE;
+	this.sirinaPojedinca = window.innerWidth/this.energija;	
+	this.dnoEkrana = window.innerHeight - this.VISINA_MASE;		// treba nova visina
 }	// Igrac
+
+
+Igrac.prototype.praviMasu = function() {
+	var ljudi = [];
+	for(var i = 0; i < this.energija; i++){
+		var slikaPojedinca = new Image();
+		slikaPojedinca.src = "slike/politicar.png";
+		slikaPojedinca = prilagodiSliku(this.standard, slikaPojedinca)
+		var slucaj = Math.random() * 60 - 30;
+		ljudi.push({
+			slika: slikaPojedinca, 
+			slucajnoOdstupanje: slucaj
+		})
+	}
+	return ljudi;
+}	// praviMasu()
 
 
 Igrac.prototype.crtaMasu = function() {	
@@ -17,17 +33,3 @@ Igrac.prototype.crtaMasu = function() {
 		this.sadrzaj.drawImage(this.ljudi[i].slika, polozajX, this.dnoEkrana)
 	}	
 }	// crtaMasu()
-
-
-Igrac.prototype.praviMasu = function() {
-	var ljudi = [];
-	for(var i = 0; i < this.energija; i++){
-		var pojedinac = new Image();
-		pojedinac.src = "slike/politicar.png";
-		// prilagodi sliku
-		var slucaj = Math.random() * 60 - 30;
-		ljudi.push( {slika: pojedinac, slucajnoOdstupanje: slucaj} )
-	}
-	return ljudi;
-}	// praviMasu()
-
