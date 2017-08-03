f86d4cb961c6de2bff2c027af6217b70ffea3cd4
Damjan Pavlica <mudroljub@gmail.com>
2017-08-03 12:04:00 +0200

    Update logika.js

--- js/logika.js
+++ js/logika.js
@@ -1,29 +1,3 @@
-/*****************************************************************
-    URADITI:
-* ubaciti sirinu i visinu ekrana medju parametre, da se ne uzima window.innerHeight tokom igre
-	* prvo mora da ucita pozadinu da bi prilagodio
-* da crtabalon posle paradajza
-// parola da skida energiju
-// bacaParolu malo nakon izlaska, a nekad ne baca
-// politicar izadje, malo odstoji, pa ode
-* da crta balon posle paradajza, kao masu
-* menjanje oruzja
-* uvodna animacija uvecavanje skupstina
-* prikazati najbolji rezultat u tabeli (napraviti upisivanje)
-* prilagodiPozadinu, uzeti u obzir sire i tanje ekrane
-* izdvojiti klasu Politicar
-* prilagoditi tablu s poenima
-
-    PROBLEMI:
-* parole se preklapaju jer izadje po jedna za svakog politicara
-* prvi paradajz ne treba da puca
-* da crtaProjektilNaLiku ne napusta prozor, a crtaParadajzOkolo ne ulazu u zauzet prozor
-* kad je presirok ekran, sece pozadinu po visini !
-
-    DOBRA PRAKSA:
-* zaokruziti crtanje na pun piksel, bez decimala
-********************************************************************/
-
 // nazivi bitni, od njih pravi objekte
 var slike = {
     pozadine: {
